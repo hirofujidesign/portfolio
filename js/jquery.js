9baee58e66c10df757ed756af4e672a9797beefc
unknown <hirofujidesign@gmail.com>
2020-09-25 05:12:54 -0400

    'contactpagefix'

--- js/jquery.js
+++ js/jquery.js
@@ -77,26 +77,3 @@ $(document).ready(function () {
 
 
 });
-
-//   if (mouseX > 325 &&  mouseX < 1000 && mouseY < 90 && mouseY >500){
-//     $('.product-hero-image.middle').show();
-//     $('.product-hero-image.left').hide();
-//     $('.product-hero-image.right').hide();
-//     $('.product-hero-image.bottom').hide();
-//     $('.product-hero-image.bottom-left').hide();
-//     $('.product-hero-image.bottom-right').hide();
-//     $('.product-hero-image.top').hide();
-//     $('.product-hero-image.top-left').hide();
-//     $('.product-hero-image.top-right').hide();
-//   }
-//   else if (mouseX < imageLeft) {
-//     $('.product-hero-image.left').show();
-//     $('.product-hero-image.middle').hide();
-//     $('.product-hero-image.right').hide();
-//   }
-//   else {
-//     $('.product-hero-image.right').show();
-//     $('.product-hero-image.middle').hide();
-//     $('.product-hero-image.left').hide();
-//   }
-
